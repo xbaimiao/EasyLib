43a0a87c48dc64667fecc11d93cf1ff42e558c63
小白 <3104026189@qq.com>
2023-03-27 19:51:11 +0800

    新增注解命令

--- src/main/kotlin/com/xbaimiao/easylib/module/command/command.kt
+++ src/main/kotlin/com/xbaimiao/easylib/module/command/command.kt
@@ -17,3 +17,32 @@ data class ArgNode(
 val onlinePlayers: ArgNode = ArgNode("player", exec = { token ->
     Bukkit.getOnlinePlayers().map { it.name }.filter { it.uppercase().startsWith(token.uppercase()) }
 })
+
+fun registerCommand(clazz: Class<*>): Boolean {
+    val header = clazz.getAnnotation(CommandHeader::class.java) ?: return false
+
+    val subCommands = ArrayList<CommandSpec>()
+
+    for (declaredField in clazz.declaredFields) {
+        if (declaredField.getAnnotation(CommandBody::class.java) != null) {
+            val commandSpec = declaredField.get(clazz) as CommandSpec
+            subCommands.add(commandSpec)
+        }
+    }
+
+    command(header.command) {
+        if (header.description.isNotEmpty()) {
+            description = header.description
+        }
+        if (header.permission.isNotEmpty()) {
+            permission = header.permission
+        }
+        if (header.permissionMessage.isNotEmpty()) {
+            permissionMessage = header.permissionMessage
+        }
+        subCommands.forEach {
+            sub(it)
+        }
+    }.register()
+    return true
+}
